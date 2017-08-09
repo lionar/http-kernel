7aa38dc6f2be7b5160f7801b418b245df9504326
Aron Wouters <lionar@mail.com>
2017-08-09 11:37:59 +0200

    Kernel now using firestark app.

--- source/kernel.php
+++ source/kernel.php
@@ -3,57 +3,35 @@
 namespace http;
 
 use firestark\app;
-use exception;
 
 class kernel
 {
-    private $routes = [ ];
-    private $app = null;
-    
-    public function __construct ( array $routes, app $app )
-    {
-        $this->routes = $routes;
-        $this->app = $app;
-    }
-    
-    public function handle ( request $request ) : response
-    {
-        $response = $this->call ( ( string ) $request );
-        if ( ! $response instanceOf response )
-            $response = $this->interpret ( $response );
-        return $response;
-    }
-    
-    private function interpret ( $result ) : response
-    {
-        // make checking if route is existent 
-        // more explicit.
-        return ( $result === null ) ?
-            $this->handleMissing ( ) :
-            $this->handleArbitrary ( $result );
-    }
-    
-    private function call ( string $request )
-    {
-        if ( array_key_exists ( $request, $this->routes ) )
-            return $this->app->call ( $this->routes [ $request ] );
-    }
-    
-    private function handleMissing ( ) : response
-    {
-        return $this->respond ( '<h1>The resource you where looking for does not exist.</h1>', 404, 'text/html' );
-    }
-    
-    private function handleArbitrary ( $result ) : response
-    {
-        if ( is_string ( $result ) )
+	private $app = null;
+
+	public function __construct ( app $app )
+	{
+		$this->app = $app;
+	}
+
+	public function handle ( request $request ) : response
+	{
+		if ( ! $this->app [ 'router' ]->has ( ( string ) $request ) )
+			return $this->handleMissing ( $request );
+		
+		$value = $this->app->call ( $this->app [ 'router' ]->match ( ( string ) $request ) );
+		return $this->createResponse ( $value );
+	}
+
+	private function createResponse ( $result ) : response
+	{
+		 if ( is_string ( $result ) )
             return $this->handleString ( $result );
         if ( is_array ( $result ) )
             return $this->handleArray ( $result );
         throw new exception ( 'The return value of your route can not be handled by the kernel.' );
-    }
-    
-    private function handleString ( string $result ) : response
+	}
+
+	private function handleString ( string $result ) : response
     {
         $type = ( $result !== strip_tags ( $result ) ) 
             ? 'text/html' : 'text/plain';
@@ -65,8 +43,13 @@ class kernel
     {
         return $this->respond ( json_encode ( $result ), 200, 'application/json' );
     }
-    
-    private function respond ( string $content, int $status, string $type ) : response
+
+	private function handleMissing ( $request ) : response
+	{
+		return $this->respond ( '<h1>The resource you where looking for does not exist.</h1>', 404, 'text/html' );
+	}
+
+	private function respond ( string $content, int $status, string $type ) : response
     {
         $response = new response ( $content, $status );
         $response [ 'Content-Type' ] = $type;
